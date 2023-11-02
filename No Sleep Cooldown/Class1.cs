31d05a3ad2cdc746c2ffe7c5847fbd5e1bdf4f12
Zach O <zachcatlover23@outlook.com>
2023-11-02 05:23:49 -0500

    Add project files.

---
+++ No Sleep Cooldown/Class1.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using TLDLoader;
+
+namespace NoSleepCooldown
+{
+    public class NoSleepCooldown : Mod
+    {
+        public override string ID => "YourModUniqueID";
+
+        public override string Name => "Your Mod Name";
+
+        public override string Author => "Your Name";
+
+        public override string Version => "1";
+
+        public override void Update()
+        {
+            mainscript.M.minTimeBeforeSleep = 0;
+        }
+    }
+
+}
+
